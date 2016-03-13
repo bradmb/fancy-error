b80a8d3827ff7684feb775b58844273c3493614d
Brad Butner <bradbutner83@gmail.com>
2016-03-12 17:31:52 -0700

    Made it so the config was setup to init without requiring a global.asax entry

--- InternalData.cs
+++ InternalData.cs
@@ -5,7 +5,7 @@ namespace FancyError
 {
     internal static class InternalData
     {
-        internal static ClientConfiguration Configuration;
+        internal static ClientConfiguration Configuration = new ClientConfiguration();
         internal static string ErrorPageTemplate;
 
         internal static Dictionary<string, ErrorData> Encounters = new Dictionary<string, ErrorData>();
